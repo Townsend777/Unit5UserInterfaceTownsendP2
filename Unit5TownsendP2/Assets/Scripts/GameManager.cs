b620ac4fde5cb454edd454d624a10f3e29aab05d
Bribri18 <122388718+Townsend777@users.noreply.github.com>
2023-04-19 10:00:38 -0700

    Yes Yes
    Yes

--- Unit5TownsendP2/Assets/Scripts/GameManager.cs
+++ Unit5TownsendP2/Assets/Scripts/GameManager.cs
@@ -9,13 +9,16 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -31,6 +34,13 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
         }
+    } 
+
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
