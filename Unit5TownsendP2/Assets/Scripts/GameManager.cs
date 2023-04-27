c33859b13d31f12e7f73192ae477a0db69d0a061
Bribri18 <122388718+Townsend777@users.noreply.github.com>
2023-04-27 09:58:04 -0700

    I'm Done
    I did it

--- Unit5TownsendP2/Assets/Scripts/GameManager.cs
+++ Unit5TownsendP2/Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
@@ -16,6 +16,8 @@ public class GameManager : MonoBehaviour
     public Button restartButton;
     public GameObject titleScreen;
     public bool isGameActive;
+    public GameObject pauseScreen;
+    private bool paused;
 
     public TextMeshProUGUI LivesText;
     private int lives;
@@ -23,36 +25,41 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    } 
+        //Check if the user has pressed the p key 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
+
+    }
 
     IEnumerator SpawnTarget()
     {
-        while(isGameActive)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
 
         }
-    } 
+    }
 
-   public void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
-    }  
+    }
 
     public void UpdateLives(int livesToChange)
     {
         lives += livesToChange;
-        LivesText.text = "Lives: " + lives; 
+        LivesText.text = "Lives: " + lives;
         if (lives <= 0)
         {
             GameOver();
@@ -64,13 +71,13 @@ public class GameManager : MonoBehaviour
         restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
-    } 
+    }
 
     public void RestartGame()
     {
-        
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    } 
+    }
 
     public void StartGame(int difficulty)
     {
@@ -85,4 +92,20 @@ public class GameManager : MonoBehaviour
 
         titleScreen.gameObject.SetActive(false);
     }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        } 
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
+    } 
 }
